bfd43fd30db614e8cd369cb0e1d9482fc513bf93
coleaderme <114289459+coleaderme@users.noreply.github.com>
2024-05-13 19:35:09 +0530

    Start app

--- app/main.py
+++ app/main.py
@@ -18,10 +18,14 @@ def uuid_table():
     uuid_rows = generate_uuids()
     return render_template('uuid-table.html', uuid_rows=uuid_rows)
 
-
+# functions can be declared anywhere now with Flask.
 def generate_uuids(count=25):
     rows = []
     for i in range(1, count+1):
         row = (i, str(uuid.uuid4()), bool(random.randrange(0, 2)))
         rows.append(row)
     return rows
+
+# Start the app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
